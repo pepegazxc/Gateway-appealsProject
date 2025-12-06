2fadeb07909194763631b5eaec6d74bfdc3202e2
pepega <werewrs555@gmail.com>
2025-12-06 19:56:12 +0300

    global initial commit

---
+++ src/main/java/gateway/GatewayAppealsProjectApplication.java
@@ -0,0 +1,13 @@
+package gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayAppealsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayAppealsProjectApplication.class, args);
+    }
+
+}
