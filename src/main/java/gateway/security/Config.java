c7e3e48ec6c8f3513a4c1e71a663c371efb3f1ce
pepegazxc <werdewrs555@gmail.com>
2026-02-01 17:15:31 +0300

    feature(jwt) : Added secret key and decoder

--- src/main/java/gateway/security/Config.java
+++ src/main/java/gateway/security/Config.java
@@ -1,16 +1,27 @@
 package gateway.security;
 
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.websocket.Decoder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.SecretKey;
 
 @Configuration
 @EnableWebSecurity
 public class Config {
+
+    @Value("${token.key}")
+    private String jwtSecret;
 
     @Bean
     public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) {
@@ -23,5 +34,12 @@ public class Config {
                         .jwt(Customizer.withDefaults()));
 
         return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(){
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
+        SecretKey spec = Keys.hmacShaKeyFor(keyBytes);
+        return NimbusJwtDecoder.withSecretKey(spec).build();
     }
 }
